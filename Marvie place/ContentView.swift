9e44bd453c336ea74f9908270a3569113cf82258
Temiloluwa <91268094+Ytemiloluwa@users.noreply.github.com>
2022-08-26 19:15:59 +0100

    initial commit

--- Marvie place/ContentView.swift
+++ Marvie place/ContentView.swift
@@ -13,7 +13,15 @@ struct ContentView: View {
         Text("Hello World")
     }
 }
-
+struct rounded : Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: [.topLeft,.topRight], cornerRadii: CGSize(width: 35, height: 35))
+        
+        return Path(path.cgPath)
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
